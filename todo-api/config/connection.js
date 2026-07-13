9a3cc1e8f969ff050993b2cabc09cbc765319ee5
HassanTanoliOff <hassantanolioff@gmail.com>
2026-07-13 16:13:59 +0500

    Remove some commented code

--- todo-api/config/connection.js
+++ todo-api/config/connection.js
@@ -16,14 +16,3 @@ const DBconnect = process.env.connectionString;
 }
 
 module.exports = mdbConnection;
-
-// const mdbConnection = async ()=>{
-//       try {
-//         await mongoose.connect(DBconnect);
-//         console.log("Connected to MongoDB");
-//       } catch (err) {
-//         console.log({
-//           message: `Failed to connect to DB Error:${err.message}`,
-//         });
-//       }
-// }
